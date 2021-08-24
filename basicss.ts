14452076ca850cca2525ef4c681a85a43743c597
NIRMITHA <avishkanirmitha@gmail.com>
2021-08-24 19:49:50 +0530

    basic of type script

---
+++ basicss.ts
@@ -0,0 +1,40 @@
+// prmitive
+let age:number;
+
+age=12;
+
+let userName:string='hello';
+
+let bool:boolean=true;
+let empty:boolean=false;
+
+// reffrence  types (arrays , object)
+
+let hobbies:string[];
+
+hobbies=['play','cricket'];
+
+let person:{
+    name:string;
+    age:number;
+};
+
+person={
+    name:'Max',
+    age:32,
+    // village:'matara'
+};
+
+
+// arrays + ibjects
+let peoples:{ // 1) declare
+    name:string,
+    age:number,
+}[];
+
+peoples=[{ // 2) assign value
+  name:'Max',
+  age:32,
+  // village:'matara'
+}];
+
---
+++ withTypescript.js
@@ -0,0 +1,11 @@
+function add(a, b) {
+    return a + b;
+}
+var result = add(2, 5);
+console.log(result);
+console.log('hello  kohomada');
+var peoples1 = [{
+        name: 'Max',
+        age: 32
+    }];
+console.log(peoples1);
